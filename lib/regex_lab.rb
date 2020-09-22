95309919dc7566f032f3bcacff049a18f90721a5
J K <kj688895@gmail.com>
2020-09-22 21:43:29 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,7 +4,7 @@ def starts_with_a_vowel?(word)
 
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\bun+\z+ing/) 
+  text.scan(/\bun+\w+ing/) 
 end
 
 def words_five_letters_long(text)
